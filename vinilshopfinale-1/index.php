f87bfb19b60c94ac8d77bedb6728dd27bb5666e2
giona007 <giona.bongiovanni.al@gcaruso.edu.it>
2026-05-26 19:34:16 +0200

    Update index.php

--- vinilshopfinale-1/index.php
+++ vinilshopfinale-1/index.php
@@ -1,14 +1,15 @@
-<?php  
+<?php
 /*
  * index.php
- * Home page semplice che mostra immagini di esempio e un link rapido
- * al catalogo. Usa le funzioni dei partials per head, header e footer.
+ * Home page: mostra tre immagini di artisti e un pulsante
+ * che porta al catalogo.
  */
-include ('confing.php');
+include('confing.php');
 include('partials/template.php');
 include('partials/header.php');
 include('partials/footer.php');
-
+ 
+// Gestisce il logout inviato dal form nell'header
 if (isset($_POST['logout'])) {
     $_SESSION = [];
     session_destroy();
@@ -16,27 +17,27 @@ if (isset($_POST['logout'])) {
     exit;
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="it">
 <head>
     <?php renderHead('VinilShop - Home'); ?>
 </head>
 <body>
-
+ 
     <?php renderHeader('home'); ?>
-
+ 
+    <!-- Tre immagini degli artisti in evidenza -->
     <div class="artisti">
         <img src="immagini/faneto.png" alt="Faneto" class="artista">
-        <img src="immagini/artie.png" alt="Artie" class="artista">
-        <img src="immagini/shiva.png" alt="Shiva" class="artista">
+        <img src="immagini/artie.png"  alt="Artie"  class="artista">
+        <img src="immagini/shiva.png"  alt="Shiva"  class="artista">
     </div>
-
+ 
+    <!-- Pulsante per andare al catalogo -->
     <div class="scopri">
         <a href="catalogo.php" class="btn-scopri">Scopri di più</a>
     </div>
-
+ 
     <?php renderFooter(); ?>
-
 </body>
 </html>
